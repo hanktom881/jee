9a2d5d2806c827c619e1d795efc71f5bad9bd66d
Hank Tom <hanktom@gmail.com>
2015-07-21 14:37:22 +0800

    initial commit

---
+++ atm/src/com/tom/LoginServlet.java
@@ -0,0 +1,53 @@
+package com.tom;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userid = request.getParameter("userid");
+		String pw = request.getParameter("pw");
+		if (userid != null && pw != null) {
+			if (userid.equals("jack") && pw.equals("1234")) {
+				response.getWriter().append("1");
+			} else {
+				response.getWriter().append("0");
+			}
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
