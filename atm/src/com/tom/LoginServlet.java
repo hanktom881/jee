c35097f6d352dfe0733521588eba265ef12131e7
Hank Tom <hanktom@gmail.com>
2015-07-28 15:52:17 +0800

    註冊, 苦工

--- atm/src/com/tom/LoginServlet.java
+++ atm/src/com/tom/LoginServlet.java
@@ -1,11 +1,19 @@
 package com.tom;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -31,11 +39,52 @@ public class LoginServlet extends HttpServlet {
 		String userid = request.getParameter("userid");
 		String pw = request.getParameter("pw");
 		if (userid != null && pw != null) {
+			Connection conn = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager
+					.getConnection("jdbc:mysql://j.snpy.org/j104?user=j104&password=104jabc");
+				/*Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("select * from users where "
+						+ "userid = '"+userid+"' and pw='"+pw+"'");*/
+				String sql = "select * from users where userid = ? and pw = ?";
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, userid);
+				pstmt.setString(2, pw);
+				ResultSet rs = pstmt.executeQuery();
+				boolean pass = rs.next();
+				if (pass){
+//					response.getWriter().append("1");
+					response.sendRedirect("success.jsp"); 
+				}else{
+//					response.getWriter().append("0");
+					HttpSession session = request.getSession();
+					session.setAttribute("error", "登入失敗,請重新登入");
+					response.sendRedirect("login.jsp");
+				}
+				rs.close();
+				pstmt.close();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			/*
 			if (userid.equals("jack") && pw.equals("1234")) {
 				response.getWriter().append("1");
 			} else {
 				response.getWriter().append("0");
-			}
+			}*/
 		}
 
 	}
